0a5ab947d22c620b065db9d8735e0d9c80b62954
Jeffrey UTFPR <jeffrey@alunos.utfpr.edu.br>
2023-11-16 14:08:02 -0300

    add trackingservices

--- src/main/java/utfpr/trabalho/api/controller/TrackingController.java
+++ src/main/java/utfpr/trabalho/api/controller/TrackingController.java
@@ -2,12 +2,14 @@ package utfpr.trabalho.api.controller;
 
 
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import utfpr.trabalho.api.model.tracking.Tracking;
 import utfpr.trabalho.api.model.user.UsersModel;
 import utfpr.trabalho.api.repository.TrackingRepository;
 import utfpr.trabalho.api.repository.UsersRepository;
+import utfpr.trabalho.api.service.TrackingService;
 
 import java.util.List;
 
@@ -19,6 +21,7 @@ public class TrackingController {
 
     private final TrackingRepository trackingRepository;
 
+    private TrackingService trackingService;
 
     @PostMapping("/save")
     public ResponseEntity<Tracking> save(@RequestBody Tracking tracking){
@@ -30,8 +33,8 @@ public class TrackingController {
         return ResponseEntity.ok(trackingRepository.findAll());
     }
 
-
-
-
-
+    @GetMapping("/{code}")
+    public String trackingObject(@PathVariable String code) {
+        return trackingService.trackingOneObject(code);
+    }
 }
