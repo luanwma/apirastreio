32c6f5d281697b96d8768929baa50cb14184a738
Jeffrey UTFPR <jeffrey@alunos.utfpr.edu.br>
2023-11-16 18:03:32 -0300

    add automation logic

--- src/main/java/utfpr/trabalho/api/service/QuartzService.java
+++ src/main/java/utfpr/trabalho/api/service/QuartzService.java
@@ -4,13 +4,27 @@ import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import static java.lang.System.*;
 
 @Component
 public class QuartzService implements Job {
 
+
+
     public void execute(JobExecutionContext context) {
+
+        LocalDateTime localDataTime = LocalDateTime.now();
         out.println("Execute Quartz Service - 1 minute!!");
+
+        // disparar consultas de objetos
+
+        // if(localDataTime.isAfter(LocalDateTime token.getTimeExpiration())) {
+        // adicionar condicional pra refazer o job do token
+            out.println(localDataTime.format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss")));
+       // }
     }
 
 }
